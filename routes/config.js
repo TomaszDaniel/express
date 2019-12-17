c4a90db488eb3208ac96b031917769e0bb7d45f2
Tomasz Daniel <tomaszz.daniel@gmail.com>
2019-12-18 00:32:52 +0100

    config db

--- routes/config.js
+++ routes/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:QJ94yaox7JNjExyk@cluster0-wdxvl.mongodb.net/test?retryWrites=true&w=majority',
     keySession: ['TWOJKLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000
 }
