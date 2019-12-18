014cb72536c1d65da806533571487a2b7a30816c
Tomasz Daniel <tomaszz.daniel@gmail.com>
2019-12-19 00:26:21 +0100

    api news

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const indexRouter = require('./routes/index');
 const newsRouter = require('./routes/news');
 const quizRouter = require('./routes/quiz');
 const adminRouter = require('./routes/admin');
+const apiRouter = require('./routes/api');
 
 
 const app = express();
@@ -45,6 +46,7 @@ app.use('/', indexRouter);
 app.use('/news', newsRouter);
 app.use('/quiz', quizRouter);
 app.use('/admin', adminRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
